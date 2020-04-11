2c0620e5c490616437e8d4f905c3ccd3ce3b2b31
Felipe <felipe.pereira.dev@gmail.com>
2020-04-11 15:20:00 -0300

    Updates

---
+++ precache-manifest.b5fc1bedc240f63764195f365c9010a1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925939d90a4d4aa910d3c03e5be7f0cf",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "992f3996111ba5ae8b66",
+    "url": "/memory-game/static/css/main.39be8339.chunk.css"
+  },
+  {
+    "revision": "c0eb6e5376a6f65a3d1c",
+    "url": "/memory-game/static/js/2.626b5586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.626b5586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992f3996111ba5ae8b66",
+    "url": "/memory-game/static/js/main.4f8c371b.chunk.js"
+  },
+  {
+    "revision": "7ba82378826073dce3ea",
+    "url": "/memory-game/static/js/runtime-main.02752f75.js"
+  },
+  {
+    "revision": "d529815cca66e9f61443aaea0e9dfdc9",
+    "url": "/memory-game/static/media/c.d529815c.png"
+  },
+  {
+    "revision": "3cc11e4baaa4ed06473404048511b4f7",
+    "url": "/memory-game/static/media/css.3cc11e4b.png"
+  },
+  {
+    "revision": "aa52c9bf96db9d0515ebfabd39274bf5",
+    "url": "/memory-game/static/media/html.aa52c9bf.png"
+  },
+  {
+    "revision": "6a3eeaf05c2edb097d7909c21d0b8c07",
+    "url": "/memory-game/static/media/java.6a3eeaf0.png"
+  },
+  {
+    "revision": "cf90a72e7a6dffd32226cf8e2ca3a351",
+    "url": "/memory-game/static/media/php.cf90a72e.png"
+  },
+  {
+    "revision": "2fd7faa020a19d438f74c08468fea5e1",
+    "url": "/memory-game/static/media/r.2fd7faa0.png"
+  },
+  {
+    "revision": "3c692fcfc22bcbadcef1caef9ba66319",
+    "url": "/memory-game/static/media/react.3c692fcf.png"
+  },
+  {
+    "revision": "fd6c3258270a1a15dd62310912bd6507",
+    "url": "/memory-game/static/media/ruby.fd6c3258.png"
+  },
+  {
+    "revision": "98eb9ccb4770e7f98429b3affaa97ac6",
+    "url": "/memory-game/static/media/sql.98eb9ccb.png"
+  },
+  {
+    "revision": "ed36ddc8ce018210f1482b0ccb93f7dd",
+    "url": "/memory-game/static/media/swift.ed36ddc8.png"
+  }
+]);
